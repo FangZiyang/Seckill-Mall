be04e90ea88dc44b762bbaa906f3666d631b785c
Ryan_Fzy <fangziyangryan@gmail.com>
2024-03-17 16:53:35 +0800

    begin hello web

--- src/main/java/com/miaoshaproject/APP.java
+++ src/main/java/com/miaoshaproject/APP.java
@@ -1,7 +1,23 @@
 package com.miaoshaproject;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
 public class APP {
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello web";
+    }
+
     public static void main(String[] args) {
         System.out.println("hello web word!");
+        SpringApplication.run(APP.class, args);
+
     }
 }
